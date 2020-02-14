b8b7e3bdb4e707d96bd697e9216b88aa896c3cfd
Payton Swick <payton@foolord.com>
2020-02-14 17:55:38 -0500

    Clarify doc that getUpgradePlanSlugFromPath checks for upgradeability

--- client/state/selectors/get-upgrade-plan-slug-from-path.js
+++ client/state/selectors/get-upgrade-plan-slug-from-path.js
@@ -8,10 +8,12 @@ import canUpgradeToPlan from 'state/selectors/can-upgrade-to-plan';
 /**
  * Given a route (path) fragment used to indicate a plan, return the corresponding plan slug.
  *
+ * Only returns the plan slug if the site can be upgraded to that plan.
+ *
  * @param  {object}   state      Global state tree
  * @param  {number}   siteId     The site we're interested in upgrading
  * @param  {string}   path       The path fragment indicating the plan we want to upgrade to
- * @returns {string|null}        The plan slug that corresponds to the given path, or null if the site cannot be upgraded or the plans are not populated
+ * @returns {string|null}        The plan slug that corresponds to the given path, or null if the site cannot be upgraded or the state is not populated yet
  */
 export default function getUpgradePlanSlugFromPath( state, siteId, path ) {
 	return (
