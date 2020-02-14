6b9e1011858e721842361ec94c7b0d0c3c551cf9
retrofox <rdsuarez@gmail.com>
2020-02-14 15:54:08 -0300

    themes: show switching hoepage warning from list

--- client/my-sites/themes/single-site-jetpack.jsx
+++ client/my-sites/themes/single-site-jetpack.jsx
@@ -14,6 +14,7 @@ import CurrentTheme from 'my-sites/themes/current-theme';
 import SidebarNavigation from 'my-sites/sidebar-navigation';
 import FormattedHeader from 'components/formatted-header';
 import ThanksModal from 'my-sites/themes/thanks-modal';
+import SwitchingHomepageModal from 'my-sites/themes/switching-homepage-modal';
 import config from 'config';
 import JetpackReferrerMessage from './jetpack-referrer-message';
 import JetpackUpgradeMessage from './jetpack-upgrade-message';
@@ -114,6 +115,7 @@ const ConnectedSingleSiteJetpack = connectOptions( props => {
 				{ siteId && <QuerySitePlans siteId={ siteId } /> }
 				{ siteId && <QuerySitePurchases siteId={ siteId } /> }
 				<ThanksModal source={ 'list' } />
+				<SwitchingHomepageModal source={ 'list' } />
 				{ showWpcomThemesList && (
 					<div>
 						<ConnectedThemesSelection
